241c57ed887b32582c1e8b80e1b0de048227aafd
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-06-14 18:12:43 -0700

    Adds a favicon

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
+      favicon: "./src/favicon.ico",
       template: "./src/index.html",
       filename: "./index.html"
     })
